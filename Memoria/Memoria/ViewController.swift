e30eaf4422fcc4385cc5b4367766b336783e9b72
omaestra <omaestra@gmail.com>
2019-07-17 20:59:22 +0200

    - Code refactor.

--- Memoria/Memoria/ViewController.swift
+++ Memoria/Memoria/ViewController.swift
@@ -27,15 +27,24 @@ class ViewController: UIViewController {
         self.countLabel.text = "Count: \(repeticiones)"
         self.countLabel.textColor = #colorLiteral(red: 0.3984073997, green: 0.7686031461, blue: 1, alpha: 1)
     }
+    
+    private func flipCard(emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle(nil, for: .normal)
+            button.backgroundColor = UIColor.black
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.orange
+        }
+    }
 
     @IBAction func cardPressed(_ sender: UIButton) {
         repeticiones += 1
-        let index = buttons.index(of: sender)!
         
-        DispatchQueue.main.async {
-            sender.setTitle(self.emojiChoices[index], for: .normal)
-            sender.backgroundColor = sender.backgroundColor == UIColor.black ? UIColor.orange : UIColor.black
-            sender.setTitle(sender.backgroundColor ==  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ? nil : self.emojiChoices[index], for: .normal)
+        if let cardIndex = buttons.firstIndex(of: sender) {
+            flipCard(emoji: emojiChoices[cardIndex], on: sender)
+        } else {
+            debugPrint("Choosen card does not exit.")
         }
     }
 
